e31e62d7856003c7159c18ff6652a8c6fda9eba3
Party4Bread <solsam10@naver.com>
2017-11-13 14:57:13 +0900

    missed?
    maybe

---
+++ static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "pwntools",
+    version: "stable",
+    language: "kr",
+    subprojects: {},
+    canonical_url: "http://party4bread.github.io/pwntools-docs-KR/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "cca1083f"
+  };
+  
